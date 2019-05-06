a8f9656a1f65aed713be905662395ac1b9700640
joseph1506 <josephin.angel@gmail.com>
2019-05-06 07:46:55 +0530

    standalone app

---
+++ src/main/java/com/joe/elsk/ElasticStandaloneApplication.java
@@ -0,0 +1,13 @@
+package com.joe.elsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticStandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticStandaloneApplication.class, args);
+    }
+
+}
